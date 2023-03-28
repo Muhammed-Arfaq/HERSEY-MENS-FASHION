13d3c1d7d7c97bbe87e20d2c6891221de853d458
Muhammed Arfaq R V <arfaq20@gmail.com>
2023-03-28 15:42:06 +0530

    mongodb connection updated

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const app = require('./app')
-const catchAsync = require('./utils/catchAsync')
 
 dotenv.config({ path: './config.env' })
 
 const db = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
 
-const connectDB = catchAsync(async () => {
+const connectDB = async () => {
     await mongoose.connect(db, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true
     })
-    mongoose.connection
-        .once("open", () => console.log("database connected successfully"))
-        .on("error", error => {
-            console.log("error: ", error);
-        })
+}
+
+mongoose.connection
+    .once("open", () => console.log("database connected successfully"))
+    .on("error", error => {
+        console.log("error: ", error);
 })
 
 connectDB().then(() => {
